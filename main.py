99f2421904f72ed28859086ef28d644523f0b5b2
Danil Kantsulin <danil.kantsulin@raftds.com>
2024-10-16 18:32:25 +0300

    add nahuy

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from database import create_database, delete_database
+from router import router as task_ruter
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    await create_database()
+    yield
+    await delete_database()
+
+
+app = FastAPI(
+    title="CURD_KA_PAIN", lifespan=lifespan
+)
+app.include_router(task_ruter)
